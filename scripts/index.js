20824219a65d371f1596a30b1faffaca85d58388
Collin Orendorff <co2fc@virginia.edu>
2025-07-11 17:19:55 -0400

    Added button functionality for the modals; smooth transitions in and out of view for modals; javascript for modal input fields

--- scripts/index.js
+++ scripts/index.js
@@ -9,8 +9,11 @@ editProfileBtn.addEventListener("click", function () {
 
 editProfileCloseBtn.addEventListener("click", function () {
     editProfileModal.classList.toggle("modal_is-opened");
+    //these 2 lines ensure changes to fields aren't saved if X is pressed
+    profileNameInput.value = currentProfileName.textContent;
+    profileDescriptionInput.value = currentProfileDescription.textContent;
 });
-
+/*------------------------------------------------*/
 // "New Post" modal selections and class editing
 const newPostBtn = document.querySelector(".profile__plus-button");
 const newPostModal = document.querySelector("#new-post-modal");
@@ -23,3 +26,43 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
     newPostModal.classList.toggle("modal_is-opened");
 });
+/*------------------------------------------------*/
+//Filling form modals when opening "edit profile" modal
+//Selecting input fields in edit profile modal
+const profileNameInput = editProfileModal.querySelector("#profile-name");
+const profileDescriptionInput = editProfileModal.querySelector("#profile-description");
+//Selecting text content currently in profile
+const currentProfileName = document.querySelector(".profile__name");
+const currentProfileDescription = document.querySelector(".profile__description");
+//Setting value of input fields
+profileNameInput.value = currentProfileName.textContent;
+profileDescriptionInput.value = currentProfileDescription.textContent;
+/*------------------------------------------------*/
+//Edit Profile Form submission
+const editProfileForm = document.querySelector("#edit-profile-form");
+
+function handleProfileFormSubmit(evt) {
+    evt.preventDefault();
+    currentProfileName.textContent = profileNameInput.value;
+    currentProfileDescription.textContent = profileDescriptionInput.value;
+    editProfileModal.classList.toggle("modal_is-opened");
+}
+
+editProfileForm.addEventListener('submit', handleProfileFormSubmit);
+/*------------------------------------------------*/
+//New Post form submission
+const newPostForm = document.querySelector("#new-post-form");
+const linkInput = document.querySelector('#image-link');
+const captionInput = document.querySelector('#caption-input');
+
+function handleNewPostSubmit(evt) {
+    evt.preventDefault();
+
+    console.log(linkInput.value);
+    console.log(captionInput.value);
+    newPostModal.classList.toggle("modal_is-opened");
+
+    evt.target.reset();
+}
+
+newPostForm.addEventListener('submit', handleNewPostSubmit);
