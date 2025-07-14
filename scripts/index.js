35bd4ce6583018929d57303549ed834e40d9f423
Collin Orendorff <co2fc@virginia.edu>
2025-07-14 16:48:50 -0400

    Added array containing card data in objects; implemented basic forEach loop to iterate through this new array of objects

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+//Array for storing "card" objects and their data
+const initialCards = [
+    {
+        name: 'Val Thorens',
+        link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg'
+    },
+    {
+        name: 'Restaurant terrace',
+        link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg'
+    },
+    {
+        name: 'An outdoor cafe',
+        link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg'
+    },
+    {
+        name: 'A very long bridge, over the forest and through the trees',
+        link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg'
+    },
+    {
+        name: 'Tunnel with morning light',
+        link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg'
+    },
+    {
+        name: 'Mountain house',
+        link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg'
+    },
+];
 // "Edit Profile" modal selections and class editing
 const editProfileBtn = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
@@ -75,3 +102,7 @@ function openModal(modal) {
 function closeModal(modal) {
     modal.classList.remove("modal_is-opened");
 }
+//Looping through array of cards "initialCards"
+initialCards.forEach(function (card) {
+    console.log(card.name);
+});
