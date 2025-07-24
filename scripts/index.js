fbe3053b1983e03a223389917bc61e46d3a28e1d
Collin Orendorff <co2fc@virginia.edu>
2025-07-23 21:15:46 -0400

    added: JS form validation, UX improvements (closing modal upon clicking outside of it or pressing Esc)

--- scripts/index.js
+++ scripts/index.js
@@ -35,17 +35,32 @@ const editProfileBtn = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-button");
 const editProfileIcon = document.querySelector(".profile__edit-icon");
+const editProfileForm = document.querySelector("#edit-profile-form");
+const editProfileSubmitButton = editProfileForm.querySelector(".modal__save-button");
+const editProfileErrorSpan = editProfileForm.querySelector("modal__error");
+const profileNameInput = editProfileModal.querySelector("#profile-name");
+const profileDescriptionInput = editProfileModal.querySelector("#profile-description");
 
 //edit profile modal close button functionality
 editProfileBtn.addEventListener("click", () => {
     openModal(editProfileModal);
+    //function below is declared in validation.js; this is put to ensure button is enabled
+    //upon opening (since validation file is loaded in before this .js file)
+    enableButton(editProfileSubmitButton, selectors);
+    //hideInputError() declared in validation.js
+    //these calls are used to get rid of error messages from prior non-submitted inputs since 
+    //values of these input fields are reset to match current profile upon closing with "X"
+    hideInputError(editProfileForm, profileNameInput, selectors);
+    hideInputError(editProfileForm, profileDescriptionInput, selectors);
 });
 
 editProfileCloseBtn.addEventListener("click", () => {
     closeModal(editProfileModal);
     //these 2 lines ensure changes to fields aren't saved if X is pressed
-    profileNameInput.value = currentProfileName.textContent;
-    profileDescriptionInput.value = currentProfileDescription.textContent;
+    //setting delay so user doesn't see field resetting until modal is fully gone
+    setTimeout(() => {
+        resetEditFormFields();
+    }, 300);
 });
 
 /*------------------------------------------------*/
@@ -53,6 +68,7 @@ editProfileCloseBtn.addEventListener("click", () => {
 const newPostBtn = document.querySelector(".profile__plus-button");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-button");
+const newPostForm = document.querySelector("#new-post-form");
 
 newPostBtn.addEventListener("click", () => {
     openModal(newPostModal);
@@ -63,18 +79,17 @@ newPostCloseBtn.addEventListener("click", () => {
 });
 /*------------------------------------------------*/
 //Filling form modals when opening "edit profile" modal
-//Selecting input fields in edit profile modal
-const profileNameInput = editProfileModal.querySelector("#profile-name");
-const profileDescriptionInput = editProfileModal.querySelector("#profile-description");
 //Selecting text content currently in profile
 const currentProfileName = document.querySelector(".profile__name");
 const currentProfileDescription = document.querySelector(".profile__description");
 //Setting value of input fields
-profileNameInput.value = currentProfileName.textContent;
-profileDescriptionInput.value = currentProfileDescription.textContent;
+function resetEditFormFields() {
+    profileNameInput.value = currentProfileName.textContent;
+    profileDescriptionInput.value = currentProfileDescription.textContent;
+}
+resetEditFormFields();
 /*------------------------------------------------*/
 //Edit Profile Form submission
-const editProfileForm = document.querySelector("#edit-profile-form");
 
 function handleProfileFormSubmit(evt) {
     evt.preventDefault();
@@ -86,9 +101,9 @@ function handleProfileFormSubmit(evt) {
 editProfileForm.addEventListener('submit', handleProfileFormSubmit);
 /*------------------------------------------------*/
 //New Post form submission
-const newPostForm = document.querySelector("#new-post-form");
-const linkInput = document.querySelector('#image-link');
-const captionInput = document.querySelector('#caption-input');
+const linkInput = newPostForm.querySelector('#image-link');
+const captionInput = newPostForm.querySelector('#caption-input');
+const newPostSubmitButton = newPostForm.querySelector(".modal__save-button");
 
 function handleNewPostSubmit(evt) {
     evt.preventDefault();
@@ -104,8 +119,11 @@ function handleNewPostSubmit(evt) {
     cardsContainer.prepend(cardToInsert);
 
     closeModal(newPostModal);
-
-    evt.target.reset();
+    //setting delay so user doesn't see resetting until modal is fully gone
+    setTimeout(() => {
+        evt.target.reset();
+        disableButton(newPostSubmitButton, selectors);
+    }, 300);
 }
 
 newPostForm.addEventListener('submit', handleNewPostSubmit);
@@ -113,10 +131,13 @@ newPostForm.addEventListener('submit', handleNewPostSubmit);
 //functions for changing visibility (opening/closing) of modals above
 function openModal(modal) {
     modal.classList.add("modal_is-opened");
+    //callback function in this listener allows user to close current modal with Esc
+    window.addEventListener('keydown', escapeToClose);
 }
 
 function closeModal(modal) {
     modal.classList.remove("modal_is-opened");
+    window.removeEventListener('keydown', escapeToClose);
 }
 
 //Selecting the card template and cards container
@@ -186,3 +207,31 @@ const previewCloseBtn = previewModal.querySelector(".modal__close-button_type_pr
 previewCloseBtn.addEventListener('click', () => {
     closeModal(previewModal);
 })
+
+//Closing any of the modals by clicking outside of the modal (area with darkened background)
+const modalList = document.querySelectorAll(".modal");
+modalList.forEach(modal => {
+    modal.addEventListener('click', (evt) => {
+        if (evt.target.classList.contains("modal")) closeModal(modal);
+        if (evt.target.id === "edit-profile-modal") {
+            setTimeout(() => {
+                resetEditFormFields();
+            }, 300);
+        }
+    });
+});
+
+//function for closing modal by pressing Escape key
+//listener added to modal in openModal() function, listener removed in closeModal() function
+function escapeToClose(evt) {
+    //only one modal can have this class at once
+    const currentOpenModal = document.querySelector(".modal_is-opened");
+    if (evt.keyCode === 27) {
+        closeModal(currentOpenModal);
+        if (currentOpenModal.id === "edit-profile-modal") {
+            setTimeout(() => {
+                resetEditFormFields();
+            }, 300);
+        }
+    }
+}
---
+++ scripts/validation.js
@@ -0,0 +1,87 @@
+// Declaring a configuration object that contains the
+// necessary classes and selectors. 
+const selectors = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__save-button",
+  inactiveButtonClass: "modal__save-button_disabled",
+  inputErrorClass: "modal__input_type_error",
+}
+
+//makes visible (by adding text) an error <span> element corresponding to a specific field
+//and gives it the browser-provided error message to display
+function showInputError(formElement, inputElement, message, selectors) {
+    const errorSpanId = "#" + inputElement.id + "-error";
+    const errorSpanElement = formElement.querySelector(errorSpanId);
+    errorSpanElement.textContent = message;
+    inputElement.classList.add(selectors.inputErrorClass);
+}
+
+//makes invisible (by clearing text) an error <span> element corresponding to a specific field
+function hideInputError(formElement, inputElement, selectors) {
+    const errorSpanId = "#" + inputElement.id + "-error";
+    const errorSpanElement = formElement.querySelector(errorSpanId);
+    errorSpanElement.textContent = "";
+    inputElement.classList.remove(selectors.inputErrorClass);
+}
+
+//checks if specific input field has status of "valid" and accordingly hides or shows it
+function checkInputValidity(formElement, inputElement, selectors) {
+    if (!inputElement.validity.valid) {
+        showInputError(formElement, inputElement, inputElement.validationMessage, selectors);
+    } else {
+        hideInputError(formElement, inputElement, selectors);
+    }
+}
+
+//checks if at least one input field in array of input fields is "invalid"
+function hasInvalidInput(inputList) {
+    return inputList.some(inputElement => {
+        return !inputElement.validity.valid;
+    });
+}
+
+//wrote below enable and disable functions so that they can also be reused in index.js
+function disableButton(buttonElement, selectors) {
+    buttonElement.disabled = true;
+    buttonElement.classList.add(selectors.inactiveButtonClass);
+}
+
+function enableButton(buttonElement, selectors) {
+    buttonElement.disabled = false;
+    buttonElement.classList.remove(selectors.inactiveButtonClass);
+}
+
+function toggleButtonState(inputList, buttonElement, selectors) {
+    if (hasInvalidInput(inputList)) {
+        disableButton(buttonElement, selectors);
+    } else {
+        enableButton(buttonElement, selectors);
+    }
+}
+
+//iterates through inputs in each field and gives them event listeners to perform validation
+function setEventListeners(formElement, selectors) {
+    const inputList = Array.from(formElement.querySelectorAll(selectors.inputSelector));
+    const buttonElement = formElement.querySelector(selectors.submitButtonSelector);
+
+    toggleButtonState(inputList, buttonElement, selectors);
+
+    inputList.forEach(inputElement => {
+        inputElement.addEventListener('input', () => {
+            checkInputValidity(formElement, inputElement, selectors);
+            toggleButtonState(inputList, buttonElement, selectors);
+        });
+    });
+}
+
+//calls setEventListeners on all modal__form elements
+function enableValidation(selectors) {
+    const formList = document.querySelectorAll(selectors.formSelector);
+    formList.forEach(formElement => {
+        setEventListeners(formElement, selectors);
+    });
+}
+
+//runs nested validation functions
+enableValidation(selectors);
