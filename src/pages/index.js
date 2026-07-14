36111afe77b486fb3e1b8fa1a9dbf72d6264ffbc
Collin Orendorff <co2fc@virginia.edu>
2026-07-13 20:26:12 -0400

    Removed one last reference to validation.js which is no longer used

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import "./index.css";
-import { enableValidation, selectors } from "../scripts/validation.js";
+// import { enableValidation, selectors } from "../scripts/validation.js";
 import heartLiked from "../images/heart-liked.svg";
 import heartDefault from "../images/heart-default.svg";
 import binHovered from "../images/bin-hovered.svg";
@@ -327,7 +327,6 @@ previewCloseBtn.addEventListener("click", () => {
   closeModal(previewModal);
 });
 
-// Edit Avatar opening, closing, and form submission handling
 editAvatarBtn.addEventListener("click", () => {
   openModal(editAvatarModal);
 });
@@ -372,4 +371,3 @@ function escapeToClose(evt) {
   }
 }
 
-enableValidation(selectors);
